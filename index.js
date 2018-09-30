56d1f0065138c7321f62f2b9a81725d045e455b0
CodeSalvageO <40140954+CodeSalvageO@users.noreply.github.com>
2018-09-30 04:14:59 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("save5"),
+value = button.form.save3.value;
+value2 = button.form.save4.value;
+button.onclick = function() {
+    foo(value);
+    foo(value2);
+}
